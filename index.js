6bc116c8c1007193ebc8789026c3c2fe3afd520e
FezHacker <yehuda.lelah@gmail.com>
2023-03-15 23:35:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-import { getWifiNetworks } from './wifi_networks.py';
-import { getWifiPassword } from './wifi_password.py';
-
 const networksDropdown = document.getElementById("networks");
 const passwordField = document.getElementById("password");
 const resultDiv = document.querySelector(".result");
 
-fetch("wifi_networks.py")
-    .then(response => response.text())
-    .then(script => eval(script))
-    .then(networks => {
-        networks.forEach(network => {
-            const option = document.createElement("option");
-            option.text = network.ssid;
-            networksDropdown.add(option);
-        });
-    })
-    .catch(error => console.error(error));
+// Contents of wifi_networks.py
+const networks = [
+    { ssid: "Network 1", security: "WPA2" },
+    { ssid: "Network 2", security: "WEP" },
+    { ssid: "Network 3", security: "Open" }
+];
+
+// Contents of wifi_password.py
+function getWifiPassword(ssid) {
+    switch (ssid) {
+        case "Network 1":
+            return "password1";
+        case "Network 2":
+            return "password2";
+        case "Network 3":
+            return "password3";
+        default:
+            return "No password found for network";
+    }
+}
+
+networks.forEach(network => {
+    const option = document.createElement("option");
+    option.text = network.ssid;
+    networksDropdown.add(option);
+});
 
 networksDropdown.addEventListener("change", () => {
     const selectedNetwork = networksDropdown.value;
-    fetch(`wifi_password.py?ssid=${selectedNetwork}`)
-        .then(response => response.text())
-        .then(password => {
-            passwordField.textContent = password;
-            resultDiv.style.display = "block";
-        })
-        .catch(error => console.error(error));
+    const password = getWifiPassword(selectedNetwork);
+    passwordField.textContent = password;
+    resultDiv.style.display = "block";
 });

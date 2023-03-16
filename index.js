f2dbbc520ce1c63ab03e7c63a68facf82f58947f
FezHacker <yehuda.lelah@gmail.com>
2023-03-15 22:56:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const networksDropdown = document.getElementById("networks");
+const passwordField = document.getElementById("password");
+const resultDiv = document.querySelector(".result");
+
+fetch("wifi_networks.py")
+    .then(response => response.text())
+    .then(script => eval(script))
+    .then(networks => {
+        networks.forEach(network => {
+            const option = document.createElement("option");
+            option.text = network.ssid;
+            networksDropdown.add(option);
+        });
+    })
+    .catch(error => console.error(error));
+
+networksDropdown.addEventListener("change", () => {
+    const selectedNetwork = networksDropdown.value;
+    fetch(`wifi_password.py?ssid=${selectedNetwork}`)
+        .then(response => response.text())
+        .then(password => {
+            passwordField.textContent = password;
+            resultDiv.style.display = "block";
+        })
+        .catch(error => console.error(error));
+});
